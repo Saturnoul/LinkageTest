052fcc9a4c8dcac2ae9fe4301dfd6ec303577484
Saturnluohao <3099916791@qq.com>
2019-10-18 10:21:20 +1200

    Test docker registry

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -f Dockerfile -t linkage:test .
-sudo yum install -y expect
+sudo apt-get install -y expect
 expect login.sh
 docker tag linkage:test ccr.ccs.tencentyun.com/saturn/linkage:test
 docker push ccr.ccs.tencentyun.com/saturn/linkage:test
