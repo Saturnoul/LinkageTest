3f3549ac36f6b85aff40a88889328711bd096199
Saturnluohao <3099916791@qq.com>
2019-10-19 10:05:58 +0800

    Test ali

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 docker build -f Dockerfile -t linkage:test .
 sudo apt-get install -y expect
 #expect login.sh
-docker login --username=saturnluohao registry.cn-shanghai.aliyuncs.com --password=cmhdbwsngp6
+sudo docker login --username=saturnluohao registry.cn-shanghai.aliyuncs.com --password=cmhdbwsngp6
 #docker tag linkage:test ccr.ccs.tencentyun.com/saturn/linkage:test
 #docker push ccr.ccs.tencentyun.com/saturn/linkage:test
-docker tag linkage:test registry-vpc.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
-docker push registry-vpc.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
+docker tag linkage:test registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
+docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
