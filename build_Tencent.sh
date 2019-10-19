58d80d90e0e6c4a627dd508c07f6149000686482
Saturnluohao <3099916791@qq.com>
2019-10-19 11:00:59 +0800

    Test direct tencent

--- build_Tencent.sh
+++ build_Tencent.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 docker build -f Dockerfile -t linkage:test .
 sudo apt-get install -y expect
-expect login_Tencent.sh
+docker login --username=100008604578 ccr.ccs.tencentyun.com --password=@cmhdb_wsngp6
 docker tag linkage:test ccr.ccs.tencentyun.com/saturn/linkage:test
 docker push ccr.ccs.tencentyun.com/saturn/linkage:test
