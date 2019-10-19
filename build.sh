3bef831fe58725817de711b171f827675e24c295
Saturnluohao <3099916791@qq.com>
2019-10-19 10:20:45 +0800

    Test shit

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker build -f Dockerfile -t linkage:test .
-#sudo apt-get install -y expect
-#expect login.sh
-sudo docker login --username=saturnluohao registry.cn-shanghai.aliyuncs.com --password=cmhdbwsngp6
+sudo apt-get install -y expect
+expect login.sh
+#sudo docker login --username=saturnluohao registry.cn-shanghai.aliyuncs.com --password=cmhdbwsngp6
 #docker tag linkage:test ccr.ccs.tencentyun.com/saturn/linkage:test
 #docker push ccr.ccs.tencentyun.com/saturn/linkage:test
-sudo docker tag linkage:test registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
-sudo docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
+docker tag linkage:test registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
+docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
--- login.sh
+++ login.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/expect
+set timeout 3
 #spawn docker login --username=100008604578 ccr.ccs.tencentyun.com
 #expect "*Password:"
 #send "@cmhdb_wsngp6\r"
 #interact
-#spawn docker login --username=saturnluohao registry-vpc.cn-shanghai.aliyuncs.com
-#expect "Password:"
-#send "cmhdbwsngp6\r"
-#interact
+spawn docker login --username=saturnluohao registry.cn-shanghai.aliyuncs.com
+expect "Password:"
+send "cmhdbwsngp6"
+interact
