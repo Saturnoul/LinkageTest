4e9fb32393e37971ebb2d0f0b4ed873cb2f0be3f
Saturnluohao <3099916791@qq.com>
2019-10-19 10:17:35 +0800

    Test shit

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ docker build -f Dockerfile -t linkage:test .
 sudo docker login --username=saturnluohao registry.cn-shanghai.aliyuncs.com --password=cmhdbwsngp6
 #docker tag linkage:test ccr.ccs.tencentyun.com/saturn/linkage:test
 #docker push ccr.ccs.tencentyun.com/saturn/linkage:test
-docker tag linkage:test registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
-docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
+sudo docker tag linkage:test registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
+sudo docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
