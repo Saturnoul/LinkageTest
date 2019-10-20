50975417753d8d3c069a6b136ddf85e10aec5961
Saturnluohao <3099916791@qq.com>
2019-10-20 09:49:55 +0800

    This time my own registry

--- build_Ali.sh
+++ build_Ali.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-docker build -f Dockerfile -t linkage:latest .
-sudo apt-get install -y expect
-expect login_Ali.sh
-docker tag linkage:latest registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
-docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
+
+#docker build -f Dockerfile -t linkage:latest .
+#sudo apt-get install -y expect
+#expect login_Ali.sh
+#docker tag linkage:latest registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
+#docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
+
+docker build -f Dockerfile -t linkage:latest
+docker tag linkage:latest www.saturnluo.cn/linkage:latest
+docker push wwww.saturnluo.cn/linkage:latest
