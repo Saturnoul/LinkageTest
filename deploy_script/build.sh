f9e951f64a9e09f3edb3f9155ba0978296addf47
Saturnluohao <3099916791@qq.com>
2019-10-20 12:49:34 +0800

    Finally everything done!

--- deploy_script/build.sh
+++ deploy_script/build.sh
@@ -2,7 +2,7 @@
 
 docker build -f Dockerfile -t linkage:latest .
 sudo apt-get install -y expect
-expect ./login.sh
+expect deploy_script/login.sh
 docker tag linkage:latest saturnluo/linkage:latest
 docker push saturnluo/linkage:latest
 
