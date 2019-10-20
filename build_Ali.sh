f5faf0f4905c5d4e368e9500350358ecc8ef1144
Saturnluohao <3099916791@qq.com>
2019-10-20 10:26:54 +0800

    Shit

--- build_Ali.sh
+++ build_Ali.sh
@@ -9,6 +9,6 @@
 
 docker build -f Dockerfile -t linkage:latest .
 docker images
-docker tag linkage:latest 49.235.244.95/linkage:test
+docker tag linkage:latest 49.235.244.95:443/linkage:test
 docker images
-docker push 49.235.244.95/linkage:test
+docker push 49.235.244.95:443/linkage:test
