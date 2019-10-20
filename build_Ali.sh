fa4539f779a3d5a018ffae44d82d4924e073efdc
Saturnluohao <3099916791@qq.com>
2019-10-20 09:27:41 +0800

    Test Ali

--- build_Ali.sh
+++ build_Ali.sh
@@ -2,5 +2,5 @@
 docker build -f Dockerfile -t linkage:test .
 sudo apt-get install -y expect
 expect login_Ali.sh
-docker tag linkage:test registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
-docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:test
+docker tag linkage:latest registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
+docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
