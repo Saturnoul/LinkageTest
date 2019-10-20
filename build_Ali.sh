174be8ce66f2367f88fc55a900d50260fc862d36
Saturnluohao <3099916791@qq.com>
2019-10-20 10:58:01 +0800

    Back to Ali

--- build_Ali.sh
+++ build_Ali.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-#docker build -f Dockerfile -t linkage:latest .
-#sudo apt-get install -y expect
-#expect login_Ali.sh
-#docker tag linkage:latest registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
-#docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
-
-
 docker build -f Dockerfile -t linkage:latest .
-docker images
-docker tag linkage:latest www.saturnluo.cn/linkage:test
-docker images
-docker pull www.saturnluo.cn/alpine:latest
-docker push www.saturnluo.cn/linkage:test
+sudo apt-get install -y expect
+expect login_Ali.sh
+docker tag linkage:latest registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
+docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
+
+
+#docker build -f Dockerfile -t linkage:latest .
+#docker tag linkage:latest www.saturnluo.cn/linkage:test
+#docker push www.saturnluo.cn/linkage:test
