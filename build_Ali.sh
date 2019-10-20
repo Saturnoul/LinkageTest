7928293356e48bb8abf5e510e4e14e6d8f6bbf26
Saturnluohao <3099916791@qq.com>
2019-10-20 11:20:00 +0800

    Test for docker hub

--- build_Ali.sh
+++ build_Ali.sh
@@ -3,8 +3,8 @@
 docker build -f Dockerfile -t linkage:latest .
 sudo apt-get install -y expect
 expect login_Ali.sh
-docker tag linkage:latest registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
-docker push registry.cn-shanghai.aliyuncs.com/saturnluo/linkage:latest
+docker tag linkage:latest saturnluo/linkage:latest
+docker push saturnluo/linkage:latest
 
 
 #docker build -f Dockerfile -t linkage:latest .
--- login_Ali.sh
+++ login_Ali.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/expect
+
+#set timeout 3
+#spawn docker login --username=saturnluohao registry.cn-shanghai.aliyuncs.com
+#expect "Password:"
+#send "cmhdbwsngp6\r"
+#interact
+
 set timeout 3
-spawn docker login --username=saturnluohao registry.cn-shanghai.aliyuncs.com
-expect "Password:"
-send "cmhdbwsngp6\r"
+spawn docker login --username=saturnluo
+expect "*Password:"
+send "@cmhdb_wsngp6\r"
 interact
